dac62b0e29c25f2de68d0e8e48f423173b77c5e3
saokeems <saokeems@gmail.com>
2026-09-11 11:33:55 +0700

    add tailwindcss, axios and cors deps

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import { products } from "./fakeDB/fakeProducts.js";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.use((req, res, next) => {
